935869752895986b28b9caaec1fbd420c4b72ecf
ihmaisrael@gmail.com <ihmaisrael@gmail.com>
2020-09-25 16:49:10 -0500

    Para subir lo de la clase del lunes

---
+++ c1.py
@@ -0,0 +1,14 @@
+print(1e3) #1x10^3
+print(2.5e3) # ->2500
+
+viga = 1000 #10 metros
+vigaAproximada = 1050 # medida aproximada
+
+eAbs = abs(viga - vigaAproximada)
+print(f'El error absoluto es {eAbs}')
+
+eRelativo = eAbs / abs(viga)
+print(f'El Error relativo es {eRelativo}')
+
+eRPorcentual = eRelativo *100
+print(f'El error Relativo Porcentual es {eRPorcentual}')
