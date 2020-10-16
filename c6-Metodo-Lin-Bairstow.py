e6ff7b73b0dccf83c03c67daacb4cbcdd68f0058
ihmaisrael@gmail.com <ihmaisrael@gmail.com>
2020-10-15 23:11:49 -0500

    Ajustes en Lin-B

--- c6-Metodo-Lin-Bairstow.py
+++ c6-Metodo-Lin-Bairstow.py
@@ -4,13 +4,15 @@ print('\n \t\tMetodo de Lin Bairstown - Factores Cuadraticos')
 print('Cortesia de Diana Portillo, Alejandro Tapia e Ian Marentes que me ayudaron a recuperar el codigo')
 print('\n\n\t Los resultados tabulados son:\n')
 #Polinomio
-a=[1,-1,6,-3,4] #Coeficientes del polinomio
+#a=[1,-1,6,-3,4] #Coeficientes del polinomio Ordenados de manera Desc.
+a = [24, -26, 9, -1] 
 p=0
 q=0
 
-tol=0.01
+tol=0.1
+print(f'Para el polinomio de coeficientes: {a} \t tol={tol}\n' )
 b=copy.deepcopy(a) #Para poteger los valores de a
-n=len(a) #Numero de elementos en a
+n=len(a) #Numero de elementos en a , nos servira para el grado del polinomio
 
 while True:
     for k in range(n):
@@ -21,8 +23,8 @@ while True:
         else:
             b[k]=a[k]-p*b[k-1]-q*b[k-2]
             if(k==n-2):
-                R=b[n-2]
-                S=a[n-1]-q*b[n-3]
+                R=b[n-2] #Penultimo coeficiente
+                S=a[n-1]-q*b[n-3] #Ultimo coeficiente
     dp=R/b[n-3]
     dq=S/b[n-3]
     b_format=["%8.4g" %elem for elem in b]
